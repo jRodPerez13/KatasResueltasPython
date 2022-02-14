7e9c71dd69a4ed663a3041c929dd7432babff504
jRodPerez13 <nemesis__13@hotmail.com>
2022-02-14 13:27:10 -0600

    Katas Resueltas del Curso Intro Python - Launch X

---
+++ Modulo1Kata/program.py
@@ -0,0 +1,15 @@
+# program.py
+sum = 1 + 2
+print(sum)
+
+print('\nHola desde la consola\n')
+
+sum = 1 + 2  # 3
+product = sum * 2
+print(product)
+
+# int, plutón era considerado un planeta pero ya es muy pequeño
+planetas_en_el_sistema_solar = 8
+distancia_a_alfa_centauri = 4.367  # float, años luz
+puede_despegar = True
+transbordador_que_aterrizo_en_la_luna = "Apollo 11"  # string
